2250d3e94b1fe38f59db2bcc5c8e4c2eebf570dd
mairemare <madumaya0908@gmail.com>
2026-08-14 02:23:32 -0400

    revamped title screen

---
+++ witchy-ware/scenes/title_screen.gd
@@ -0,0 +1,35 @@
+extends Control
+
+
+@onready var main_buttons: VBoxContainer = $"main buttons"
+@onready var settings: Panel = $settings
+
+
+# Called when the node enters the scene tree for the first time.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _ready() -> void:
+	main_buttons.visible = true
+	settings.visible = false
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
+
+
+func _on_settings_pressed() -> void:
+	main_buttons.visible = false
+	settings.visible = true
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_back_pressed() -> void:
+	main_buttons.visible = true
+	settings.visible = false
